d64d5a9a546565494a45b3505d5d507cc9fba0a2
NithinNayak3 <25250963-NithinNayak3@users.noreply.replit.com>
2023-12-30 00:58:45 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        // Input
+        Console.Write("Enter the number of parking spaces: ");
+        int parkingSpaces = int.Parse(Console.ReadLine());
+        string yesterday = Console.ReadLine();
+        string today = Console.ReadLine();
+
+        int counter = 0; // Counter used to count parking spaces occupied both days
+
+        // Loop through both strings
+        for (int i = 0; i < parkingSpaces; i++)
+        {
+            if (yesterday[i] == today[i] && today[i] == 'C')
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
